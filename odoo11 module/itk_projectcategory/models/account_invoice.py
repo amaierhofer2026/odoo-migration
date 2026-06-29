4dfa9d4f7e506b6d56cfc6bc74abafc746d64c8f
amaierhofer2026 <amaierhofer2026@users.noreply.github.com>
2026-06-29 08:48:38 +0000

    Remove duplicate modules from odoo11 module/ (already in addons/)

--- odoo11 module/itk_projectcategory/models/account_invoice.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
-
-    projectcategory_id = fields.Many2one('itk_projectcategory.projectcategory', string="Project Category")
