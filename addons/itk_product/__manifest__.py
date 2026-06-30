54521e27640108ce57e4ecd406281711f63fd1de
Anna Maierhofer <anna.maierhofer@it-kommunal.at>
2026-06-30 11:34:32 +0000

    fix(itk_product): migrate to Odoo 18, fix product form fields
    - Remove # -*- coding: utf-8 -*- from all Python files
    - Update manifest: version 18.0.1.0.0, add license/installable
    - Add string="Product-Typ" label to product_type_id field
    - Remove invisible condition from subscription group (was hidden
      because default type is "consu" not "service")
    - All three fields now visible: product_type_id, recurring_invoice,
      subscription_template_id

--- addons/itk_product/__manifest__.py
+++ addons/itk_product/__manifest__.py
@@ -1,37 +1,21 @@
-# -*- coding: utf-8 -*-
 {
     'name': "itk_product",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
+    'version': '18.0.1.0.0',
+    'category': 'Uncategorized',
+    'summary': 'ITK Product extensions',
     'author': "My Company",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'product',
-                # 'itk_initial_product_import',
+                'itk_subscription',
                 ],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/itk_product.xml',
-
     ],
-    # only loaded in demonstration mode
     'demo': [
-        # 'demo/demo.xml',
     ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
