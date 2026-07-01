801a5eadc8d8ec1aad5deab6a5bad0620faeed06
amaierhofer2026 <maierhofer.anna@gmx.at>
2026-07-01 10:13:53 +0200

    fix(itk_projectcategory): migrate to Odoo 18
    - Manifest: version 0.1 -> 18.0.1.0.0, add license/installable, remove utf8 header
    - Python: remove # -*- coding: utf-8 -*- from all .py files
    - Views: <tree> -> <list>, view_type removed
    - All 26 project categories verified

--- addons/itk_projectcategory/__manifest__.py
+++ addons/itk_projectcategory/__manifest__.py
@@ -1,38 +1,20 @@
-# -*- coding: utf-8 -*-
 {
     'name': "itk_projectcategory",
-
-    'summary': """
-        Project Category""",
-
-    'description': """
-        ITK Projekt Kategorien
-    """,
-
+    'summary': "Project Category",
+    'description': "ITK Projekt Kategorien",
     'author': "Alvarium Services, Andreas Väthröder, Fabian Väthröder",
     'website': "alvarium-services.de",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'ITK - Specific Industry Applications',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    # 'depends': ['base', 'itk_crm', 'itk_data_setup', ],
-    'depends': ['base', 'account'],  # 'itk_subscription',
-
-
-    # always loaded
+    'version': '18.0.1.0.0',
+    'depends': ['base', 'account'],
     'data': [
-        # 'views/valorisierung_views.xml',
         'security/ir.model.access.csv',
         'views/account_invoice_views.xml',
+        'views/projectcategory_views.xml',
         'data/itk_projectcategory.xml',
-        
     ],
-    # only loaded in demonstration mode
-    # 'demo': [
-    #   'demo/demo.xml',
-    # ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
